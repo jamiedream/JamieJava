8ce80eced8ef6d789ffa4bfaa7c9438742a50c34
jamiedream <jamie78831@gmail.com>
2016-08-31 13:32:54 +0800

    JamieJava

--- JamieHomeWork/hw/Jamie20160830.java
+++ JamieHomeWork/hw/Jamie20160830.java
@@ -4,37 +4,37 @@ public class Jamie20160830 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-//		int []array = new int[]{0,1,2,3,4,5};//新增0-5數列
-//		int []swit;
-//		for(int i=0; i<6; i++){
-//
-//			
-//			System.out.print(array[i]+"\t");
-//		}		
-		
-		//0-5排出不重複數列
-		int []n = new int [6];
-		int []x = new int [6];
-		for(int i=0; i<n.length; i++){
-			int show;
-			boolean repeat;
-			do{
-				show = (int)(Math.random()*6);	//0-5
-				repeat = false;
-				for(int j=0; j<i; j++){
-					if(show == n[j]){
-						repeat = true;
-						break;
-					}
-				}
-			 }while(repeat);
-			n[i] = show;
-//
-			System.out.print(show+" ");
+		int []array = new int[]{0,1,2,3,4,5};//新增0-5數列
+		int []swit;
+		for(int i=0; i<6; i++){
+			for(int j=1; j<i; j++){
+			int toSwitch = (int)(Math.random()*(j));
+			System.out.println(toSwitch);
+		}		
+		}
+//		//0-5排出不重複數列
+//		int []n = new int [6];
+//		int []x = new int [6];
+//		for(int i=0; i<n.length; i++){
+//			int show;
+//			boolean repeat;
+//			do{
+//				show = (int)(Math.random()*6);	//0-5
+//				repeat = false;
+//				for(int j=0; j<i; j++){
+//					if(show == n[j]){
+//						repeat = true;
+//						break;
+//					}
+//				}
+//			 }while(repeat);
+//			n[i] = show;
+////
+//			System.out.print(show+" ");
 			
 		}
 		
 		
 	}
 
-}
+
---
+++ JamieHomeWork/hw/example.java
@@ -0,0 +1,26 @@
+package hw;
+
+public class example {
+
+	public static void main(String[] args) {
+		  int[] poker = new int[52];
+		  //initialize ordered poker number, from 1 to 52.
+		  for (int i = 0; i < poker.length; i++) {
+		   poker[i]=i+1;
+		  }
+		  
+		  for (int i = 0; i < (poker.length-1); i++) {
+		   int indexToSwitch = ((int)(Math.random() * (51-i)))+i;
+		 
+		   {
+		    int switchTemp = poker[i];
+		    poker[i] = poker [indexToSwitch];
+		    poker[indexToSwitch] = switchTemp;
+		   }
+		   
+		  }
+		  for (int i = 0; i < poker.length; i++) {
+		   System.out.print(poker[i] + " ");
+		  }
+		  }
+}
