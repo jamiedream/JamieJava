ca54c834878efb5b1cad1dd85531a6c9770ccb2b
jamiedream <jamie78831@gmail.com>
2016-09-06 09:37:37 +0800

    JamieJava

--- src/tw/org/iii/mesa0515/Drawer.java
+++ src/tw/org/iii/mesa0515/Drawer.java
@@ -6,7 +6,8 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-
+import java.util.HashMap;
+import java.util.LinkedList;
 
 import javax.swing.JPanel;
 
@@ -19,6 +20,8 @@ public class Drawer extends JPanel{
 //	}
 //	//少使用paint
 	
+	private LinkedList<HashMap<String, Integer>> line;
+	//一條linkedLIST構成的線上hashmap為線上點
 	public Drawer(){
 		//偵測滑鼠位置
 		MyListener listener = new MyListener();
@@ -26,7 +29,7 @@ public class Drawer extends JPanel{
 		//偵測滑鼠press, release, click
 		addMouseMotionListener(listener);
 		//偵測滑鼠移動事件
-		
+		line = new LinkedList<>();
 	}
 	
 	
@@ -37,7 +40,16 @@ public class Drawer extends JPanel{
 		Graphics2D g2d = (Graphics2D)g;
 		g2d.setColor(Color.blue);
 		g2d.setStroke(new BasicStroke(5));
-		g2d.drawLine(0, 0, 100, 100);	
+//		g2d.drawLine(0, 0, 100, 100);	
+		for(int i=1; i<line.size(); i++){
+			HashMap<String, Integer> p0 = line.get(i-1); 
+			HashMap<String, Integer> p1 = line.get(i); 
+			Integer p0x = p0.get("x"), p0y = p0.get("y");
+			Integer p1x = p1.get("x"), p1y = p1.get("y");
+			g2d.drawLine(p0x, p0y, p1x, p1y);
+		}
+		
+		
 	}
 
 	private class MyListener extends MouseAdapter{
@@ -46,21 +58,30 @@ public class Drawer extends JPanel{
 			// TODO Auto-generated method stub
 			super.mousePressed(e);
 			int x = e.getX(), y = e.getY();
-			System.out.println("Pressed: "+ x + "x" + y);
+//			System.out.println("Pressed: "+ x + "x" + y);
+			HashMap<String, Integer> point= new HashMap<>();
+			point.put("x", x); point.put("y", y);
+			//將xy帶入point,並將point帶到線中
+			line.add(point);
 		}
 		@Override
 		public void mouseReleased(MouseEvent e) {
 			// TODO Auto-generated method stub
 			super.mouseReleased(e);
 			int x = e.getX(), y = e.getY();
-			System.out.println("Released: "+ x + "x" + y);
+//			System.out.println("Released: "+ x + "x" + y);
+			HashMap<String, Integer> point= new HashMap<>();
+			point.put("x", x); point.put("y", y);
+			//將xy帶入point,並將point帶到線中
+			line.add(point);
 		}
 		@Override
 		public void mouseDragged(MouseEvent e) {
 			// TODO Auto-generated method stub
 			super.mouseDragged(e);
 			int x = e.getX(), y = e.getY();
-			System.out.println("Dragged: "+ x + "x" + y);
+//			System.out.println("Dragged: "+ x + "x" + y);
+
 		}
 		
 		
